97285eea71115e3b85ffb74420fec77e0acfd191
Pablo Volpato <pablo.volps@gmail.com>
2021-09-07 18:05:10 -0300

    Basic Static Components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,18 @@
-import React from 'react';
+import Navbar from 'components/Navbar';
+import Footer from 'components/Footer'
+import DataTable from 'components/DataTable';
 
 
 function App() {
   return (
-    <div>
+    <>
+    <Navbar/>
+    <div className="container">
       <h1 className="text-primary">Olá mundo!</h1>
+      <DataTable/>
     </div>
+    <Footer/>
+    </>
   );
 }
 
